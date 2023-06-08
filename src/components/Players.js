e51ae7a8ee8ffffc4a383aa3050f0e99824401fa
Thanh Mai <caothithanhmai.911.2017@gmail.com>
2023-06-08 08:04:15 +0700

    exercise-8

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Players } from '../shared/ListOfPlayers'
-export default class player extends Component {
-    render() {
-        return (
-            <div className='container'>
-                {Players.map((player)=>(
-                   <div className='column'>
-                   <div className='card'>
-                   <img src={player.img}/>
-                     <h3>{player.name}</h3>
-                     <p className='title'>{player.club}</p>
-                     <p><button>Detail</button></p>
-                   </div>
-                 </div>
-                ))}
-            </div>
-          )        
-    }
+export default function Player() {
+  return (
+    <div className='container'>
+      {Players.map((player) => (
+        <div className='column'>
+          <div className='card'>
+            <img src={player.img} />
+            <h3>{player.name}</h3>
+            <p className='title'>{player.club}</p>
+            <p><button>Detail</button></p>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
