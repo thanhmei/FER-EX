7854c9be9c9b5c1325887b8e99ad7f0564336f2a
Thanh Mai <caothithanhmai.911.2017@gmail.com>
2023-06-08 08:00:12 +0700

    exercise-7

---
+++ src/shared/ListOfPlayers.js
@@ -0,0 +1,9 @@
+
+export const Players =[
+    {id: 1, name: 'Cristiano Ronaldo', club: 'Manchester United', img:'assets/images/images/cr.jpg'},
+    {id: 2, name: 'Kante', club: 'Chelsea', img:'assets/images/images/kante.jpg'},
+    {id: 2, name: 'Messi', club: 'PSG', img:'assets/images/images/messi.jpg'},
+    {id: 2, name: 'Neymar', club: 'PSG',img:'assets/images/images/neymar.jpg'},
+    {id: 2, name: 'Kane', club: 'Tottemham',img:'assets/images/images/kane.jpg' },
+    {id: 2, name: 'Haaland', club: 'Manchester City', img:'assets/images/images/haaland.jpg'}
+  ];
